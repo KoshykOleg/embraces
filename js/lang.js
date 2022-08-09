6773c0db188b8188ff79510516afd2e38aae3866
KoshykOleg <koshykoleg@gmail.com>
2022-08-09 21:08:02 +0300

    Виправив косяки. додав мову в меню на моб. девайсах

---
+++ js/lang.js
@@ -0,0 +1,9 @@
+/* $(document).ready(function() {
+   $(".lang__ua1").click(function(event) {
+       $(".lang__eng1").toggleClass('active');
+    });
+    $(".lang__eng1").click(function(event) {
+      $(".lang__ua1").toggleClass('active');
+   });  
+});
+ */
--- js/participation__images.js
+++ js/participation__images.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
-   $(".owl-carousel").owlCarousel ({
-    items: 3,
-    loop: true,
-    nav: true
-   });
- });
+  $(".owl-carousel").owlCarousel ({
+   items: 3,
+   loop: true,
+   nav: true,
+   autoplay:true,
+   autoplayTimeout:3000,
+   autoplayHoverPause:true
+  });
+  $('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[3000])
+  })
+  $('.stop').on('click',function(){
+     owl.trigger('stop.owl.autoplay')
+   })
+});
